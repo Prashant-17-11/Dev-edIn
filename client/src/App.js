85aaa0026ae3e2853cb18c420e977787ce5d467b
Prashant Singh <prashantsingh.999000@gmail.com>
2022-11-02 12:29:41 +0530

    routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Landing />
-    </>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route
+          path='/register'
+          element={
+            <section className='container'>
+              <Register />
+            </section>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <section className='container'>
+              <Login />
+            </section>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
