d47def66e098a474a613204e6530e87da7c3a95f
Prashant Singh <prashantsingh.999000@gmail.com>
2022-11-02 10:17:56 +0530

    Initial cleanup and base setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+}
+
+export default App;
